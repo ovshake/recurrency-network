f4c2d0fd33425bbfe6171ba6625af385bc489201
ovshake <abhishek16005@iiitd.ac.in>
2018-12-03 22:03:28 +0530

    Added MARIMA unscaled

--- src/arima.py
+++ src/arima.py
@@ -13,9 +13,13 @@ train_set = train_data.iloc[:,1:].values
 test_set = test_data.iloc[: , 1:].values
 # print(test_set)
 # print(train_set.shape)
-sc = MinMaxScaler()
-train_set_sc = sc.fit_transform(train_set)
-test_set_sc = sc.fit_transform(test_set)
+train_sc = MinMaxScaler()
+test_sc = MinMaxScaler()
+train_set_sc = train_sc.fit_transform(train_set)
+test_set_sc = test_sc.fit_transform(test_set)
+min_test = test_sc.data_min_[-1]
+max_test = test_sc.data_max_[-1]
+print(max_test , min_test)
 num_samples = train_set_sc.shape[0]
 num_features = train_set_sc.shape[1]
 # X_train = train_set_sc[:, :6]
@@ -37,10 +41,13 @@ res = coint_johansen(train_set_sc, -1 , 1).eig
 model = VAR(endog=train_set_sc)
 model_fit = model.fit()
 prediction = model_fit.forecast(model_fit.y, steps=len(test_set_sc))
+prediction = test_sc.inverse_transform(prediction)
 yhat = prediction[: , -1]
-print(yhat)
-y_true = test_set_sc[: , -1]
-print(y_true)
+# print(yhat)
+y_true = test_set[: , -1]
+# print(y_true)
 for i in range(len(yhat)):
 	print(abs(yhat[i] - y_true[i]))
 # print(prediction)
+
+#https://www.analyticsvidhya.com/blog/2018/09/multivariate-time-series-guide-forecasting-modeling-python-codes/
